24865898bd13a623de3afdfde9a206faf8e9ca15
ChanhuiLee <mattew.chanhee.lee@gmail.com>
2015-07-30 19:53:53 +0900

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,29 @@
+def LetterChanges(str)
+
+  # code goes here
+  return str.swapcase 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
